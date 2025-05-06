76ff2a11577e43c37320428d86165b04d09e32eb
olgatim <tigercub220@gmail.com>
2025-05-06 18:34:28 +0300

    Update config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/procurement-demo',
+  assetPrefix: '/procurement-demo'
 };
 
 export default nextConfig;
